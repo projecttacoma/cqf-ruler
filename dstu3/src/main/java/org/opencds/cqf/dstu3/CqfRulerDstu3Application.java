898eb0b96f70a9f014004bb074fa9a38c68c7572
Chris Schuler <cschuler@myharmoniq.com>
2019-04-17 17:00:29 -0600

    Adding basic config for HAPI spring-boot-starter

---
+++ dstu3/src/main/java/org/opencds/cqf/dstu3/CqfRulerDstu3Application.java
@@ -0,0 +1,13 @@
+package org.opencds.cqf.dstu3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqfRulerDstu3Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CqfRulerDstu3Application.class, args);
+    }
+}
